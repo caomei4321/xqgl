9b6c91931f8383bad802d55514fc6092b88194f9
caomei4321 <43211374@qq.com>
2019-08-02 10:21:27 +0800

    修改migration兼容

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('phone')->unique();
+            $table->string('phone');
             $table->string('password');
             $table->string('remember_token', 100)->nullable();
             $table->string('open_id')->nullable();
--- database/migrations/2019_07_30_061804_create_admins_table.php
+++ database/migrations/2019_07_30_061804_create_admins_table.php
@@ -16,7 +16,7 @@ class CreateAdminsTable extends Migration
         Schema::create('admins', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('phone')->unique();
+            $table->string('phone');
             $table->string('password');
             $table->string('remember_token', 100)->nullable();
             $table->timestamps();
