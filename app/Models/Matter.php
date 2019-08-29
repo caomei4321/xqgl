ca530d37b1dd88428bfb35620def509f33749f26
cm <43211374@qq.com>
2019-08-29 09:36:06 +0800

    修改token时间，完成任务接口

--- app/Models/Matter.php
+++ app/Models/Matter.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Matter extends Model
 {
     protected $fillable = [
-        'title', 'address', 'content', 'image', 'accept_num', 'time_limit', 'work_num', 'level', 'type', 'source', 'is_reply', 'is_secret', 'contact_name', 'contact_phone', 'reply_remark', 'category_id', 'suggestion', 'approval', 'result', 'form', 'allocate'
+        'title', 'address', 'content', 'image', 'accept_num', 'time_limit', 'work_num', 'level', 'type', 'source', 'is_reply', 'is_secret', 'contact_name', 'contact_phone', 'reply_remark', 'category_id', 'suggestion', 'approval', 'result', 'form', 'allocate', 'status'
     ];
 
     public function situation()
