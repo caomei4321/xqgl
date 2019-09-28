23becfd7d74681a57f3837b8f2cd5c7ce230c284
Conanyh <ycoralh@163.com>
2019-09-28 09:16:00 +0800

    situation

--- app/Http/Controllers/Admin/SituationsController.php
+++ app/Http/Controllers/Admin/SituationsController.php
@@ -12,7 +12,7 @@ class SituationsController extends Controller
     public function index(Situation  $situation)
     {
         $situations = Situation::with(['Matter', 'User', 'Category'])->paginate(10);
-
+	
         return view('admin.situation.index', compact('situations'));
     }
 
