50495b5108d01ce8b4b06212e19ee8b602a1f482
cm <43211374@qq.com>
2019-09-29 19:02:33 +0800

    权限中间件

---
+++ app/Http/Middleware/CheckPermission.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckPermission
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        dd(auth()->user());
+
+        $routeName  = $request->route()->getName();
+
+
+
+        return $next($request);
+    }
+}
