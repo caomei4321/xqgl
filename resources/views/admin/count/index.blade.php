2e3db02350529a57abf6cfdff60391f527058d16
Conanyh <ycoralh@163.com>
2019-09-29 19:04:24 +0800

    index

--- resources/views/admin/count/index.blade.php
+++ resources/views/admin/count/index.blade.php
@@ -165,7 +165,7 @@
         <div class="col-sm-12">
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <h5>正在巡查人员</h5>
+                    <h5>近期任务情况</h5>
                 </div>
                 <div class="ibox-content" style="height: 400px;">
                     <div class="col-sm-12">
