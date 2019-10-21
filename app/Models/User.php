8d7691289288896a06cdcc787ed4837e15ae3d72
cm <43211374@qq.com>
2019-10-21 10:15:22 +0800

    接口返回多张图片字段

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ class User extends Authenticatable implements JWTSubject
 
     public function situation()
     {
-        return $this->belongsToMany('App\Models\Matter', 'user_has_matters', 'user_id', 'matter_id')->withPivot('see_image', 'information', 'status');
+        return $this->belongsToMany('App\Models\Matter', 'user_has_matters', 'user_id', 'matter_id')->withPivot('see_image', 'see_images', 'information', 'status');
     }
 
     public function patrolMatters()
