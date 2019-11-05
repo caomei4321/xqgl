0ab5ca8b16365fa34e0af3aebaf77fced7647b6d
Conanyh <ycoralh@163.com>
2019-11-05 18:02:54 +0800

    历史记录详情api

--- app/Http/Controllers/Api/ProgramUsersController.php
+++ app/Http/Controllers/Api/ProgramUsersController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Handlers\ImageUploadHandler;
+use App\Models\Matter;
 use App\Models\ProgramUser;
+use App\Models\Situation;
+use App\Models\User;
 use Doctrine\DBAL\Events;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -70,11 +73,25 @@ class ProgramUsersController extends Controller
         return $matters;
     }
 
-    public function historyMattersDetail(Request $request)
+    public function historyMattersDetail(Request $request, Matter $matter, Situation $situation, User $user)
     {
-        $matterDetail = $this->user()->matters()->situation()->find($request->id);
+        $matters = Matter::with(['Situation'])->where('id', $request->id)->first();
+        $data = [
+            'title' => $matters->title,
+            'address' => $matters->address,
+            'content' => $matters->content,
+            'status' => $matters->status,
+            'created_at' => $matters->created,
+            'image' => $matters->image,
+            'many_images' => explode(',', $matters->many_images),
+            'userName' => $matters->situation->user->name,
+            'see_image' => $matters->situation->see_image,
+            'information' => $matters->situation->information,
+            'process' => $matters->situation->status,
+            'see_images' => explode(';', $matters->see_images)
+        ];
 
-        return $matterDetail;
+        return $data;
     }
 
     public function weappUser()
